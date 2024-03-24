03f3d56d5885084c73129347ac9643bfdb63eff0
Rakesh Kumar <121437600+rakeshmandal7091@users.noreply.github.com>
2024-03-24 22:38:53 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,21 @@
+const target = document.getElementById('target');
+const yes = document.getElementById('yes');
+
+yes.addEventListener('click', () => {
+    alert("I Love you too Baby😊❤️❤️😊");
+});
+
+function moveTarget() {
+    const maxWidth = 400;
+    const maxHeight = 400;
+
+    const randomX = Math.floor(Math.random() * maxWidth);
+    const randomY = Math.floor(Math.random() * maxHeight);
+
+    target.style.left = randomX + 'px';
+    target.style.top = randomY + 'px';
+}
+
+target.addEventListener('mouseenter', function() {
+    moveTarget();
+});
